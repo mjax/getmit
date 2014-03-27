656748671871f7681207d15eeba92b6b296a1579
Edgar Gavrik <edgar.gavrik@gmail.com>
2014-03-27 13:01:59 +0200

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
 
 from core import *
+from clint import arguments
 from clint.textui import colored
+
+args = arguments.Args('status')
+
+def main():
+    if args.get(0) != None:
+        if 'help' in args.get(0):
+            print 'helping you'
+        elif 'make' in args.get(0):
+            print 'making this mit file'
+        elif 'status' in args.get(0):
+            print str(DATE) + " " + str(FULLNAME) 
+    else:
+        pass
+
+if __name__ == '__main__':
+    main()
